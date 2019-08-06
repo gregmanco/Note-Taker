d4723694df7ef61fd9ab846eaca4fd1ebb4697dd
Greg Manco <gregmanco@gmail.com>
2019-08-06 17:49:22 -0400

    Set up html routes

--- server.js
+++ server.js
@@ -25,26 +25,34 @@ app.get("/api/notes", function (req, res){
 
 app.post("/api/write_notes", function(req, res){
     
-    connection.query("INSERT INTO notes (title,body) VALUES (?,?)", [req.body.title,req.body.body] function( err, res){
+    connection.query("INSERT INTO notes (title,body) VALUES (?,?)", [req.body.title,req.body.body],function( err, res){
         if(err) {throw err
         }
-
+        console.log("you inserted a new note");
     });
 });
 
-app.delete ("api/delete_notes",function(req, res){
+app.delete ("/api/delete_notes/:id",function(req, res){
     connection.query("DELETE FROM notes WHERE id = ?", [req.params.id], function(err, result) {
         if(err){ throw err
-        return res.status(500).end();
+        // return res.status(500).end();
     }
-    else if (result.affectedRows === 0) {
+    // else if (result.affectedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+    //     return res.status(404).end();
+    //   }
+    //   res.status(200).end();
 
     });
 });
+// Route to Index.html
+app.get("/", function (req,res){
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+// Route to Notes.html
+app.get("/notes", function (req,res){
+    res.sendFile(path.join(__dirname, "notes.html"))
+})
 
 
 
